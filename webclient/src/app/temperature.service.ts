d00858aaa54a7cfafd0991444ce393023ccd79de
unknown <czajak1995@gmail.com>
2018-11-27 22:10:06 +0100

    Added downloading excel file

--- webclient/src/app/temperature.service.ts
+++ webclient/src/app/temperature.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { of, Observable } from "rxjs";
 import { AppComponent, BASE_URL } from './app.component';
@@ -13,34 +13,23 @@ export class TemperatureService {
     constructor(private http: HttpClient) { }
 
     getTemperatureInfoForDevice(id: number): Observable<TemperatureInfo> {
-        if (!AppComponent.isMock()) {
-            return this.http.get<TemperatureInfo>(BASE_URL + "temperature/info?deviceId=" + id).pipe(result => result)
-        }
-        // } else {
-        //   let temperature: number = (id+1)*20;
-        //   return of<number>(temperature).pipe(map(result => result))
-        // }
+        return this.http.get<TemperatureInfo>(BASE_URL + "temperature/info?deviceId=" + id).pipe(result => result)
     }
 
     getAllTempForDevice(id: number) {
-
-        if (!AppComponent.isMock()) {
-            return this.http.get<number[]>(BASE_URL + "temperature/all?deviceId=" + id)
-        } else {
-            let tab = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]
-            let resultTab = []
-            tab.forEach(element => {
-                resultTab.push(id * element)
-            });
-            return of<number[]>(resultTab).pipe(result => result)
-        }
+        return this.http.get<number[]>(BASE_URL + "temperature/all?deviceId=" + id)
     }
 
     getAverageTemperatures() {
-        if (!AppComponent.isMock()) {
-            return this.http.get<number[]>(BASE_URL + "temperature/avgs")
-        } else {
-        }
+        return this.http.get<number[]>(BASE_URL + "temperature/avgs")
+    }
+
+    downloadExcelFile() {
+        return this.http.get(BASE_URL + '/temperature/export', { responseType: 'blob'});
+        // const httpOptions = {
+        //     headers: new HttpHeaders({ 'responseType':  'ResponseContentType.Blob',
+        //     'Content-Type':  'application/vnd.ms-excel'})};
+        //   return this.http.get(BASE_URL + '/temperature/export', httpOptions);
     }
 
 }
